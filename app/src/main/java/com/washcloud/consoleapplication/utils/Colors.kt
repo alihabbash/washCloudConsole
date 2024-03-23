e25f3bdbc465279e44c782adc3bebe758c69e6cd
abdulmalekdery <abdullmalek@horizonscloud.net>
2024-03-23 21:01:22 +0200

    Added help design

--- app/src/main/java/com/washcloud/consoleapplication/utils/Colors.kt
+++ app/src/main/java/com/washcloud/consoleapplication/utils/Colors.kt
@@ -4,6 +4,13 @@ import androidx.compose.ui.graphics.Color
 
 val lightGrey = Color(0xFFE2E2E2)
 val screenBackground = Color(0xFFF8F8F8)
+val primaryDark = Color(0xFF1e4752)
 val secondaryColor = Color(0xFF71C490)
 val blueGradient = Color(0xFF1DA0B4)
 val unSelectedTextColor = Color(0xFF848484)
+val numbersBackground = Color(0xFFedeef8)
+val clearText = Color(0xFF848484)
+val borderColor = Color(0xFFd1d1d1)
+val fieldsTitles = Color(0xFF282828)
+val hints = Color(0xFFa0a0a0)
+val lightGreen = Color(0xFFf3f9f5)
