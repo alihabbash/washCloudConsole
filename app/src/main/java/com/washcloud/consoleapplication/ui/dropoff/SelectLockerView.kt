aa201b84abc4051a1da5116f9590adfeff28abc8
Husseinshlash <hoseen.sh.92@gmail.com>
2025-05-18 19:37:40 +0200

    fix-remove-mock-test-func

--- app/src/main/java/com/washcloud/consoleapplication/ui/dropoff/SelectLockerView.kt
+++ app/src/main/java/com/washcloud/consoleapplication/ui/dropoff/SelectLockerView.kt
@@ -150,11 +150,6 @@ fun SelectLockerView(
             viewModel.setShowAlert()
         }
     }
-    LaunchedEffect(Unit) {
-    delay(10000L)
-      //  wayBillNo = "O112503280003-1"
-        viewModel.sendCommand("1", "3");
-   }
 
     LaunchedEffect(wayBillNo) {
      if (isValidSerialNumber(wayBillNo)) {
